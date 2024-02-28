94200a87a78f693cb496f585137c5dc9aebf09a9
hwji <knjhw@daum.net>
2024-02-28 23:58:17 +0900

    module 복습

---
+++ pr_backend/app.js
@@ -0,0 +1,39 @@
+
+// Module. logger.js => showLogMessage
+
+// logger.js 모듈을 불러온다.
+require('./logger');
+
+
+
+// Module
+
+
+
+
+// 3초에 한 번씩 주기적으로
+// setInterval(() => {
+//     console.log('Node.js 연습중입니다');
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log('타임아웃 한번만 실행');
+// }, 3000);
+
+
+// 함수
+// function sayHello(name) {
+//     console.log('Hello ' + name);
+// }
+
+// sayHello('john');
+// sayHello('allice');
+
+// const v = 10;
+
+// if (v > 5) {
+//     console.log('It is a big number')
+// }
+// else {
+//     console.log('작은 숫자야!!');
+// }
---
+++ pr_backend/logger.js
@@ -0,0 +1,10 @@
+
+function showLogMessage(msg) {
+
+    console.log('--------------------');
+    console.log(`로그메세지는 ${msg}`);
+    console.log('--------------------');
+
+}
+
+module.exports.showLogMessage = showLogMessage;
